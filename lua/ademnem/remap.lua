c1b4e57707f3eb44b6966082e7f73737db61fd39
ademnem <adamhn1@uci.edu>
2024-10-27 20:13:30 -0700

    fixed remap file

--- lua/ademnem/remap.lua
+++ lua/ademnem/remap.lua
@@ -4,4 +4,4 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>vs", vim.cmd.vs)
 vim.keymap.set("n", "<leader>sp", vim.cmd.sp)
-# *" or +" for copy into clipboard?
+-- "* or "+ for copy into clipboard?
