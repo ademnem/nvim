c2b8036669e77b0c7a4de4844ae31dea7bf50f6a
ademnem <adamhn1@uci.edu>
2024-12-21 13:30:29 -0800

    fixed indentation in lazy.lua

--- lua/ademnem/lazy.lua
+++ lua/ademnem/lazy.lua
@@ -9,8 +9,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
             { out, "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
-      vim.fn.getchar()
-      os.exit(1)
+        vim.fn.getchar()
+        os.exit(1)
     end
 end
 vim.opt.rtp:prepend(lazypath)
@@ -20,7 +20,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = {
         -- import your plugins
-      { import = "ademnem.plugins" },
+        { import = "ademnem.plugins" },
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
