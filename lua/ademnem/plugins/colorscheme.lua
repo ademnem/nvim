e4501aea56a3642ecbdd5fd5c3e44132eb469b36
ademnem <adamhn1@uci.edu>
2024-12-21 16:04:40 -0800

    removed extra spaces in colorscheme.lua

--- lua/ademnem/plugins/colorscheme.lua
+++ lua/ademnem/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
 function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
-	
+
 	-- For transparent background, need a transparent terminal for this
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 
-return 
-{	
-    { 
+return
+{
+    {
 		"rose-pine/neovim",
 		as = "rose-pine",
 		lazy = false,	-- make sure to lwad this during startup if it is in the main colorscheme
@@ -18,10 +18,10 @@ return
 		config = function()
 			-- load colorscheme here
 			vim.cmd([[colorscheme rose-pine]])
-            
+
             -- ColorMyPencils()
 		end
-    },	
+    },
 }
 
 
