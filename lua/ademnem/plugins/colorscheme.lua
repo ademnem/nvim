0ae05b7fb747b02fd36e1c2ec508aaa33add4780
ademnem <adamhn1@uci.edu>
2025-01-10 01:48:07 -0800

    modified colorscheme.lua to match lazy.nvim recommendations

--- lua/ademnem/plugins/colorscheme.lua
+++ lua/ademnem/plugins/colorscheme.lua
@@ -1,27 +1,11 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	-- For transparent background, need a transparent terminal for this
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-
-return
-{
+return {
     {
-		"rose-pine/neovim",
-		as = "rose-pine",
-		lazy = false,	-- make sure to lwad this during startup if it is in the main colorscheme
-		priority = 1000, -- make sure to load this before all other start plugins
-		config = function()
-			-- load colorscheme here
-			vim.cmd([[colorscheme rose-pine]])
-
-            -- ColorMyPencils()
-		end
+        "rose-pine/neovim",
+        as = "rose-pine",
+        lazy = false,
+        priority = 1000, -- default priority is 50
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end,
     },
 }
-
-
