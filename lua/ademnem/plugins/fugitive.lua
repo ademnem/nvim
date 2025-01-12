6eb3987cfc94206522e33b7b9e14ea2dc603b063
ademnem <adamhn1@uci.edu>
2025-01-11 23:08:55 -0800

    added fugitive keymaps to fugitive.lua

--- lua/ademnem/plugins/fugitive.lua
+++ lua/ademnem/plugins/fugitive.lua
@@ -4,5 +4,18 @@ return
 	lazy = false,
     keys = {
         { "<leader>gs", vim.cmd.Git, desc = "Git" }
+        --[[ 
+        Keymappings in Fugitive
+        - s = stage
+        - u = unstage
+        - U = unstage everything
+        - X = discard changes under the cursor 
+        - dd = diff split
+        - dv = diff split but always vertical split
+        - cc = create commit
+        - ca = amend last commit and edit message
+        - ce = amend last commit without editting
+        - cw = reword last commit
+        ]]--
     },
 }
