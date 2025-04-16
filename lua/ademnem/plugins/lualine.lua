dcd62d2c7215e02ff2b4bbb46a2dfeb524887d67
ademnem <adamnam012@gmail.com>
2025-04-16 16:21:24 -0700

    added rose-pine colorscheme to lualine

--- lua/ademnem/plugins/lualine.lua
+++ lua/ademnem/plugins/lualine.lua
@@ -2,6 +2,10 @@ return {
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
-        opts = {},
+        opts = {
+            options = {
+                theme = 'rose-pine'
+            }
+        },
     }
 }
