20b1d4ef81c1a2a6a4ce0f8108f9025179847c6d
ademnem <adamhn1@uci.edu>
2025-04-18 18:43:00 -0700

    changed back to config because it is more readable

--- lua/ademnem/plugins/colorscheme.lua
+++ lua/ademnem/plugins/colorscheme.lua
@@ -3,10 +3,8 @@ return {
 		"rose-pine/neovim",
 		name = "rose-pine",
 		priority = 1000, -- default priority is 50
-		opts = function()
-			local opts = {}
+		config = function()
 			vim.cmd("colorscheme rose-pine")
-			return opts
 		end,
 	},
 }
