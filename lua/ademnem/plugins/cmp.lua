ee3b77dfa04575eebc847b9d1155e781c298dfb8
ademnem <adamnam012@gmail.com>
2025-05-20 01:27:08 -0700

    fixed function signatures

--- lua/ademnem/plugins/cmp.lua
+++ lua/ademnem/plugins/cmp.lua
@@ -26,7 +26,10 @@ return {
 
 		completion = {
 			list = { max_items = 3 },
-			documentation = { auto_show = false },
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+			},
 		},
 
 		sources = {
